a433aac25c7d611ed0d579ca061d92be281f35ef
CamiloCuenca <camilocuenca1810@gmail.com>
2024-11-25 22:37:38 -0500

    feat: agregar archivo PHP para explicar cómo definir e inicializar variables

---
+++ sintaxis basica/variables.php
@@ -0,0 +1,20 @@
+<?php 
+
+    // Declaro la varibale
+    $nombre ;
+
+    //Inicializo a variable
+    $nombre = "Juan";
+
+    // Imprimo el contenido de a variables
+    echo $nombre
+?>
+
+
+<?php 
+
+// Declaración e inicializacion en una sola linea
+$nombre = "Juan";
+
+
+?>
