8086ce3b5dfbed1bdbde02e686082d8f17123556
CamiloCuenca <camilocuenca1810@gmail.com>
2024-12-01 23:18:01 -0500

    edit: se agregaron nuevos conceptos de variables

--- sintaxis basica/variables.php
+++ sintaxis basica/variables.php
@@ -1,5 +1,20 @@
 <?php 
 
+    // Variable super global
+    $came_from = $_SERVER['HTTP_REFERER']; // esta forma puede ser insegura
+
+    $came_from_segura =htmlentities($_SERVER['HTTP_REFERER']); // Esta forma es muvho mas segura
+
+
+    // Varable global
+    global $nombreglobal;
+    $nombreglobal = "PEDRO";
+    echo"".$nombreglobal."\n";
+
+    // Variables estaticas (static)
+    static $nombreStatco = "Jose";
+    echo "".$nombreStatco."\n";
+
     // Declaro la varibale
     $nombre ;
 
@@ -7,14 +22,15 @@
     $nombre = "Juan";
 
     // Imprimo el contenido de a variables
-    echo $nombre
-?>
+    echo $nombre;
 
 
-<?php 
+    // Declaración e inicializacion en una sola linea
+    $nombre2 = "Juan2";
 
-// Declaración e inicializacion en una sola linea
-$nombre = "Juan";
-
-
+    // Declaración de asignación multiple
+    $level = $score = $time = 0;
+    echo"".$level."\n";
+    echo "".$score."\n";
+    echo "".$time."\n";
 ?>
