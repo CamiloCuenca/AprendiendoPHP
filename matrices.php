45346d56915c6978bfc215e6cf12563972b35109
CamiloCuenca <camilocuenca1810@gmail.com>
2024-12-01 23:18:41 -0500

    feat: agregar archvio PHP para explicar las matrices unidimencionales y bidimensionales

---
+++ matrices.php
@@ -0,0 +1,14 @@
+<?php
+
+// Lista (Array) de nombres de una dimensión
+$nombres = array("Juan","Camilo","Pedro","Carlos");
+echo $nombres[0]."\n";
+
+// Matriz bidimencional
+$oxo = array(
+    array("X"," ","O"),
+    array("O","O","x"),
+    array("X","O"," "),
+);
+echo $oxo[0][2];
+?>
